66352d42cf5604abe84f12f5ab6a3a9db648d756
Bill Cooperman <aquamongoose@gmail.com>
2014-08-24 15:45:48 -0400

    Fixed Josh silliness

--- week01/josh/match2.cpp
+++ week01/josh/match2.cpp
@@ -3,35 +3,33 @@
 using namespace std;
 
 string a, b;
-long long X = 24523454523451LL;
+unsigned long long X = 37;
 
-long long pow (long long a, long long b) {
+unsigned long long pow (unsigned long long a, unsigned long long b) {
   if (b == 0) return 1;
-  long long x = pow (a, b / 2);
+  unsigned long long x = pow (a, b / 2);
   x *= x;
-  if (b % 2 == 0) x *= a;
+  if (b % 2 == 1) x *= a;
   return x;
 }
 
 int main() {
   cin >> a >> b;
   int n = (int) a.size(), m = (int) b.size(), ans = 0;
-  long long bhash = 0, ahash = 0;
+  unsigned long long bhash = 0, ahash = 0;
   for (int i = 0; i < m; i++) {
     bhash *= X;
-    bhash += (long long) b[i];
+    bhash += (unsigned long long) b[i];
   }
   for (int i = 0; i < m - 1; i++) {
     ahash *= X;
-    ahash += (long long) a[i];
+    ahash += (unsigned long long) a[i];
   }
-  cout << bhash << endl;
   for (int i = m - 1; i < n; i++) {
     ahash *= X;
     ahash += a[i];
-	cout << ahash << endl;
     if (ahash == bhash) ans++;
-    ahash -= (long long) a[i - m + 1] * pow (X, m-1);
+    ahash -= ((unsigned long long) a[i - m + 1]) * pow ((unsigned long long) X, (unsigned long long) (m-1));
   }
   cout << ans << "\n";
 }
